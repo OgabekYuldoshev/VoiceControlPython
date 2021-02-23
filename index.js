0469b3ca5c085d67b7a45aa50cfcbdfc5551633d
OgabekYuldoshev <yuldashoff1@gmail.com>
2021-02-23 20:15:58 +0500

    d

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const data = require("./DataBase/iakt.json")
 const server = http.createServer(app)
 
 app.use(express.static("public"))
-app.use(express.json())
 
 app.get("/iakt", (req, res)=>{
   res.json(data)
